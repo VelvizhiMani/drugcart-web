c7339c79701913c3c16d034dcc380deea2f923b0
joseferrar <73059582+joseferrar@users.noreply.github.com>
2025-01-14 18:17:03 -1100

    commend remove

--- src/app/page.js
+++ src/app/page.js
@@ -22,7 +22,7 @@ export default function Home() {
         <Navcategory/>
          <Slider/>
         <Topcategory/> 
-        {/* <ProductGrid/> */}
+        <ProductGrid/>
         <Aromogroup/>
         <Servicegroup/>
         <Featuredpackage/>
