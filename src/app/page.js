a5abaf85d7299d6300176c39ad78257a15bc7a9c
joseferrar <73059582+joseferrar@users.noreply.github.com>
2025-01-16 08:00:25 -1100

    issue two

--- src/app/page.js
+++ src/app/page.js
@@ -5,19 +5,8 @@ export default function Home() {
   return (
     <>
       {/* <Nav/> */}
-        <Navcategory/>
-         <Slider/>
-        <Topcategory/> 
-        <ProductGrid/>
-        <Aromogroup/>
-        <Servicegroup/>
-        <Featuredpackage/>
-        <Healthhacks/>
-        <Shopbycategory/>
-        <Fameoftheday/>
-        <Blog/>
-        <Feedback/> 
-        <Customersaying/>
+        <AddEmployees/>
+         <ShowEmployees/>
     </>
   );
 }
