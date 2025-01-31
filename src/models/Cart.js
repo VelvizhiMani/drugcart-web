ccc25ee9dc7efee36cc0c118a79a745cbff6b685
VelvizhiMani <velvizhimcapec@gmail.com>
2025-02-01 00:26:13 +0400

    basic design

--- src/models/Cart.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const CartItemSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    quantity: { type: Number, required: true, default: 1 },
-}, {timestamps: true});
-
-export default mongoose.models.Cart || mongoose.model('Cart', CartItemSchema);
