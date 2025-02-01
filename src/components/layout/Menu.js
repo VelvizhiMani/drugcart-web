185e531164045c9ae7163fa02e72e44fb1093cd0
VelvizhiMani <velvizhimcapec@gmail.com>
2025-02-01 13:11:26 +0400

    menu text added

--- src/components/layout/Menu.js
+++ src/components/layout/Menu.js
@@ -17,7 +17,7 @@ const Menu = () => {
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 mr-2">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                                 </svg>
-                                <span>View Categories</span> </div>
+                                <span>View Categories Vel</span> </div>
                         </Link>
                     </div>
 
