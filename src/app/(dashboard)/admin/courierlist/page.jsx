2abc5600ded063eb045a4bf0c355dd82ae5a947f
joseferrar <73059582+joseferrar@users.noreply.github.com>
2025-02-19 03:13:58 -1100

    courier list sno added

--- src/app/(dashboard)/admin/courierlist/page.jsx
+++ src/app/(dashboard)/admin/courierlist/page.jsx
@@ -135,7 +135,7 @@ function CourierList() {
                                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
                             >
                                 <TableCell sx={{ fontFamily: rowText.fontFamily }}>
-                                    {i + 1}
+                                    {row?.sno}
                                 </TableCell>
                                 <TableCell
                                     sx={{ fontFamily: rowText.fontFamily }}
