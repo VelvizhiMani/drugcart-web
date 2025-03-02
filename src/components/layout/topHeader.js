e0e88852f4e2422fdb00828c82f250c6771000eb
joseferrar <73059582+joseferrar@users.noreply.github.com>
2025-03-02 10:51:32 -1100

    shopping cart local storage added for testing

--- src/components/layout/topHeader.js
+++ src/components/layout/topHeader.js
@@ -11,6 +11,7 @@ import { useRouter } from 'next/navigation';
 
 const TopHeader = () => {
   const router = useRouter()
+  const cart = useSelector((state) => state.cartData.items);
   const { profile } = useSelector((state) => state.profileData)
   const dispatch = useDispatch()
   const [pincode, setPincode] = useState("");
@@ -84,9 +85,16 @@ const TopHeader = () => {
                 <button type="button" className='text-white bg-bgcolor hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-1.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55'>
                   <CloudUploadIcon size="small" className='me-2' /> Upload
                 </button>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 text-black mt-1 ml-5 cursor-pointer" onClick={() => router.push('/cart')}>
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-                </svg>
+                <div className="relative" onClick={() => router.push('/cart')}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 text-black mt-1 ml-5 cursor-pointer">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                  </svg>
+                  {cart.length > 0 && (
+                    <div className="absolute top-0 right-0 left-8 inline-flex items-center justify-center w-5 h-5 bg-red-500 text-white text-xs rounded-full">
+                      {cart.length}
+                    </div>
+                  )}
+                </div>
               </div>
               <div className='flex justify-center items-center'>
                 <svg className="w-6 h-6 fill-sky-100 stroke-sky-500 stroke-2 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
