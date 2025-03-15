a501672dccfa0f8fc0f0b3534a69b79187da513a
joseferrar <73059582+joseferrar@users.noreply.github.com>
2025-03-14 20:58:45 -1100

    CustomerSaying text change

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -11,7 +11,7 @@ import ShopbyCategory from "@/components/home-page/shopbyCategory";
 import FameSection from "@/components/home-page/fameSection";
 import BlogCard from "@/components/home-page/blogCard";
 import Feedback from "@/components/home-page/feedback";
-import CustomerSaying from "@/components/home-page/customerSaying";
+import CustomerSaying from "@/components/home-page/CustomerSaying";
 import { useDispatch, useSelector } from "react-redux";
 import { getProfileService } from "@/services/profileService";
 
