bb6fcc936c380f6571107102a587d2ee48bf523b
joseferrar <73059582+joseferrar@users.noreply.github.com>
2025-03-20 12:03:41 -1100

    cart badge added

--- src/components/layout/topHeader.js
+++ src/components/layout/topHeader.js
@@ -11,6 +11,7 @@ import { useRouter } from "next/navigation";
 import PersonIcon from "@mui/icons-material/Person";
 
 const TopHeader = () => {
+  const cart = useSelector((state) => state.cartData.items);
   const router = useRouter();
   const { profile } = useSelector((state) => state.profileData);
   const dispatch = useDispatch();
@@ -86,80 +87,76 @@ const TopHeader = () => {
                 >
                   <CloudUploadIcon size="small" className="me-2" /> Upload
                 </button>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-6 text-black mt-1 ml-5"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-                  />
-                </svg>
-              </div>
-              <div className="flex justify-center items-center">
-                <div className="relative inline-block">
-                  {/* User Profile Button */}
-                  {profile?.username ? (
-                    <>
-                      <button
-                        onClick={() => setIsOpen(!isOpen)}
-                        className="flex items-center space-x-2 p-2 bg-gray-100 rounded-lg hover:bg-gray-200 transition"
-                      >
-                        <span className="bg-blue-500 text-white rounded-full p-2 text-xs">
-                          <PersonIcon className="w-2 h-2" />
-                        </span>
-                        <span className="font-medium">{profile?.username}</span>
-                      </button>
-
-                      {/* Dropdown Menu */}
-                      {isOpen && (
-                        <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg z-40">
-                          <ul className="text-gray-700">
-                            <li className="px-4 py-2 flex items-center gap-2 hover:bg-gray-100 cursor-pointer">
-                              My Profile
-                            </li>
-                            <li className="px-4 py-2 flex items-center gap-2 hover:bg-gray-100 cursor-pointer">
-                              My Orders
-                            </li>
-                            <li className="px-4 py-2 flex items-center gap-2 hover:bg-gray-100 cursor-pointer">
-                              My Prescription
-                            </li>
-                            <li className="px-4 py-2 flex items-center gap-2 hover:bg-gray-100 cursor-pointer">
-                              My Wallet
-                            </li>
-                            <li className="px-4 py-2 flex items-center gap-2 hover:bg-gray-100 cursor-pointer">
-                              Refer & Earn
-                            </li>
-                            <li className="px-4 py-2 flex items-center gap-2 text-red-500 hover:bg-gray-100 cursor-pointer" onClick={logout}>
-                              Logout
-                            </li>
-                          </ul>
-                        </div>
-                      )}
-                    </>
-                  ) : (
-                    <button
-                      type="button"
-                      className="text-white bg-bgcolor hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-1.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55"
-                      onClick={loginLink}
-                    >
-                      LogIn 🔒
-                    </button>
+                <div className="relative" onClick={() => router.push('/cart')}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 text-black mt-1 ml-5 cursor-pointer">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                  </svg>
+                  {cart?.length > 0 && (
+                    <div className="absolute top-0 right-0 left-8 inline-flex items-center justify-center w-5 h-5 bg-red-500 text-white text-xs rounded-full">
+                      {cart?.length}
+                    </div>
                   )}
+                  </div>
                 </div>
-              </div>
+                <div className="flex justify-center items-center">
+                  <div className="relative inline-block">
+                    {/* User Profile Button */}
+                    {profile?.username ? (
+                      <>
+                        <button
+                          onClick={() => setIsOpen(!isOpen)}
+                          className="flex items-center space-x-2 p-2 bg-gray-100 rounded-lg hover:bg-gray-200 transition"
+                        >
+                          <span className="bg-blue-500 text-white rounded-full p-2 text-xs">
+                            <PersonIcon className="w-2 h-2" />
+                          </span>
+                          <span className="font-medium">{profile?.username}</span>
+                        </button>
 
-              {/* <div className="flex justify-center items-center">
+                        {/* Dropdown Menu */}
+                        {isOpen && (
+                          <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg z-40">
+                            <ul className="text-gray-700">
+                              <li className="px-4 py-2 flex items-center gap-2 hover:bg-gray-100 cursor-pointer">
+                                My Profile
+                              </li>
+                              <li className="px-4 py-2 flex items-center gap-2 hover:bg-gray-100 cursor-pointer">
+                                My Orders
+                              </li>
+                              <li className="px-4 py-2 flex items-center gap-2 hover:bg-gray-100 cursor-pointer">
+                                My Prescription
+                              </li>
+                              <li className="px-4 py-2 flex items-center gap-2 hover:bg-gray-100 cursor-pointer">
+                                My Wallet
+                              </li>
+                              <li className="px-4 py-2 flex items-center gap-2 hover:bg-gray-100 cursor-pointer">
+                                Refer & Earn
+                              </li>
+                              <li className="px-4 py-2 flex items-center gap-2 text-red-500 hover:bg-gray-100 cursor-pointer" onClick={logout}>
+                                Logout
+                              </li>
+                            </ul>
+                          </div>
+                        )}
+                      </>
+                    ) : (
+                      <button
+                        type="button"
+                        className="text-white bg-bgcolor hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-1.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55"
+                        onClick={loginLink}
+                      >
+                        LogIn 🔒
+                      </button>
+                    )}
+                  </div>
+                </div>
+
+                {/* <div className="flex justify-center items-center">
                 <h2 className="text-md text-center">English</h2>
               </div> */}
+              </div>
             </div>
           </div>
-        </div>
       </section>
     </>
   );
