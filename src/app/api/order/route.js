b3879ec3620d48a75b535026bdc894b916b009a1
joseferrar <73059582+joseferrar@users.noreply.github.com>
2025-03-25 06:32:06 -1100

    get all order api added

--- src/app/api/order/route.js
+++ src/app/api/order/route.js
@@ -1,4 +1,4 @@
-import { authenticateUser } from "../../../utils/middleware";
+import { adminAuthorization, authenticateUser } from "../../../utils/middleware";
 import Order from "../../../models/Order";
 import { NextResponse } from "next/server";
 import connnectionToDatabase from "@/lib/mongodb";
@@ -38,6 +38,65 @@ export async function POST(request) {
     } catch (error) {
         return NextResponse.json(
             { error: "Internal Server Error" },
+            { status: 500 }
+        );
+    }
+}
+
+export async function GET(req) {
+    const { searchParams } = new URL(req.url);
+    const page = parseInt(searchParams.get("page")) || 1;
+    const limit = parseInt(searchParams.get("limit")) || 10;
+    const search = searchParams.get("search") || "";
+    const orderStatus = searchParams.get("orderStatus") || "";
+
+    try {
+        await connnectionToDatabase();
+        const { success, user, message } = await adminAuthorization();
+
+        if (!success) {
+            return NextResponse.json({ error: message }, { status: 401 });
+        }
+        const skip = (page - 1) * limit;
+
+        let query = {};
+
+        if (search) {
+            query.orderId = { $regex: search, $options: "i" };
+        }
+
+        if (orderStatus) {
+            query.orderStatus = orderStatus;
+        }
+
+        const OrderItems = await Order.find(query)
+            .sort({ createdAt: -1 })
+            .skip(skip)
+            .limit(limit);
+
+        const totalItems = await Order.countDocuments(query);
+        const totalPages = Math.ceil(totalItems / limit);
+
+        const ordersWithIndex = OrderItems.map((order, index) => ({
+            ...order.toObject(),
+            sno: skip + index + 1,
+        }));
+
+        return NextResponse.json(
+            {
+                all_orders: ordersWithIndex,
+                pagination: {
+                    totalItems,
+                    totalPages,
+                    currentPage: page,
+                },
+            },
+            { status: 200 }
+        );
+    } catch (error) {
+        console.error("Error fetching Order items:", error);
+        return NextResponse.json(
+            { error: "Failed to fetch Order items" },
             { status: 500 }
         );
     }
