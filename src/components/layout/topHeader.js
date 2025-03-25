5d4b116e467a815d9f459181d45358940b5a0ed0
joseferrar <73059582+joseferrar@users.noreply.github.com>
2025-03-24 13:26:11 -1100

    backend cart json changed

--- src/components/layout/topHeader.js
+++ src/components/layout/topHeader.js
@@ -12,7 +12,7 @@ import PersonIcon from "@mui/icons-material/Person";
 import { getCartService } from "@/services/cartService";
 
 const TopHeader = () => {
-  const cart = useSelector((state) => state.cartData.items);
+  const {items} = useSelector((state) => state.cartData);
   const router = useRouter();
   const { profile } = useSelector((state) => state.profileData);
   const dispatch = useDispatch();
@@ -39,6 +39,7 @@ const TopHeader = () => {
   const loginLink = () => {
     router.push("/login");
   };
+  
   return (
     <>
       <section className="mt-3 px-5 md:px-16">
@@ -93,9 +94,9 @@ const TopHeader = () => {
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 text-black mt-1 ml-5 cursor-pointer">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                   </svg>
-                  {cart?.length > 0 && (
+                  {items?.length > 0 && (
                     <div className="absolute top-0 right-0 left-8 inline-flex items-center justify-center w-5 h-5 bg-red-500 text-white text-xs rounded-full">
-                      {cart?.length}
+                      {items?.length}
                     </div>
                   )}
                 </div>
