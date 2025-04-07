9bd7d4c57f154b42530c685a59ba0a0420cab2bb
joseferrar <73059582+joseferrar@users.noreply.github.com>
2025-04-06 22:04:41 -1100

    productDetailsCard left and right section folder added

---
+++ src/components/ProductDetailsCard/leftsection/Leftsection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Leftsection() {
+  return (
+    <div>Leftsection</div>
+  )
+}
+
+export default Leftsection
---
+++ src/components/ProductDetailsCard/rightsection/Rightsection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Rightsection() {
+  return (
+    <div>Rightsection</div>
+  )
+}
+
+export default Rightsection
