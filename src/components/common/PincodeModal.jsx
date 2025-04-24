1c8ea6534de066515e6c23fcab5c1a204079b82c
joseferrar <joseferrar4@gmail.com>
2025-04-24 17:08:57 +0530

    search pincode text change

--- src/components/common/PincodeModal.jsx
+++ src/components/common/PincodeModal.jsx
@@ -32,7 +32,7 @@ function PincodeModal({ onClose }) {
                             value={query}
                             onChange={handleInput}
                             className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:ring-teal-500"
-                            placeholder="Search pincode or area"
+                            placeholder="Search your pincode"
                         />
                         <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700" onClick={checkBtn}>Check</button>
                     </div>
