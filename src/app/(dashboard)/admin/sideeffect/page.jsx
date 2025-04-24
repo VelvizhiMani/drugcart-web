fd5626d06e09a5003011bcdb2def2b473773a06a
VelvizhiMani <96263546+VelvizhiMani@users.noreply.github.com>
2025-04-25 02:36:58 +0400

    Update page.jsx

--- src/app/(dashboard)/admin/sideeffect/page.jsx
+++ src/app/(dashboard)/admin/sideeffect/page.jsx
@@ -77,7 +77,7 @@ function Sideeffect() {
           fontWeight="bold"
           sx={{ flexGrow: 1 }}
         >
-          Sideeffect
+          Side Effect
         </Typography>
         <Button
           color="secondary"
