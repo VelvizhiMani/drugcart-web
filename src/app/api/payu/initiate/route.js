a2be6b8074fa6158e806853abcdcadf9377bd162
VelvizhiMani <96263546+VelvizhiMani@users.noreply.github.com>
2025-05-05 11:38:01 +0400

    Update route.js

--- src/app/api/payu/initiate/route.js
+++ src/app/api/payu/initiate/route.js
@@ -4,7 +4,7 @@ export const runtime = "nodejs";
 import { NextResponse } from "next/server";
 import crypto from "crypto";
 
-export async function POST(req: Request) {
+export async function POST(req) {
   try {
     const body = await req.json();
     const { amount } = body;
