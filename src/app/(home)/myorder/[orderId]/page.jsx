9691cc8ef05d706af111bdd21e4ae139e3281222
joseferrar <joseferrar4@gmail.com>
2025-05-07 02:21:30 +0530

    order details fetch data

--- src/app/(home)/myorder/[orderId]/page.jsx
+++ src/app/(home)/myorder/[orderId]/page.jsx
@@ -1,12 +1,20 @@
 "use client";
 
-import { useState } from "react";
-import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+import { GetOrderOneService } from '@/services/orderService';
+import { useSelector, useDispatch } from "react-redux";
 
 export default function OrderViewPage() {
+    const { orderGetData } = useSelector((state) => state.orderData)
+    const router = useRouter()
     const { orderId } = useParams();
     const [status, setStatus] = useState("Processing");
+    const dispatch = useDispatch()
 
+    useEffect(() => {
+        dispatch(GetOrderOneService(orderId))
+    }, [orderId])
     const order = {
         id: orderId,
         date: "2025-05-03",
@@ -39,29 +47,33 @@ export default function OrderViewPage() {
                         {order.date}
                     </p>
                     <p className="font-medium">
-                        <span className="font-bold">Total Price :</span> Rs. 135
+                        <span className="font-bold">Total Price :</span> Rs. {orderGetData?.totalPrice}
                     </p>
                     <p className="font-medium">
-                        <span className="font-bold">Shipping Price :</span> 0
+                        <span className="font-bold">Shipping Price :</span> {orderGetData?.shippingPrice}
                     </p>
                     <p className="font-medium">
-                        <span className="font-bold">Tracking No :</span> FSRER34334DFGH
+                        <span className="font-bold">Tracking No :</span> {orderGetData?.trackingInfo?.trackingno ? orderGetData?.trackingInfo?.trackingno : "Not Found"}
                     </p>
                     <p className="font-medium">
-                        <span className="font-bold">Order Status :</span> {order.date}
+                        <span className="font-bold">Order Status :</span> {orderGetData?.trackingInfo?.orderStatus}
                     </p>
+                    <div className="font-medium flex">
+                        <span className="font-bold">Invoice :</span> <p className="text-blue-600 cursor-pointer ml-1 font-medium" onClick={() => router.push(`/invoice/${orderGetData?.orderId}`)}>View more</p>
+                    </div>
                     <button className="bg-blue-500 text-white font-semibold py-2 mt-3 px-4 rounded hover:bg-blue-600 shadow">
                         Payment Pending
                     </button>
                 </div>
                 <div>
                     <p className="text-[black] font-bold">Customer Info</p>
-                    <p>Velvizhi M</p>
-                    <p>velvizhimcapec@gmail.com</p>
-                    <p>8056800773</p>
-                    <p>No. 105, Vinayagar Street,</p>
-                    <p>Royapuram & post,</p>
-                    <p>Parry, Chennai, Tamilnadu, India,</p>
+                    <p>{orderGetData?.shippingInfo?.cus_name}{" "}{orderGetData?.shippingInfo?.lastname}</p>
+                    <p>{orderGetData?.shippingInfo?.email}</p>
+                    <p>{orderGetData?.shippingInfo?.phone}</p>
+                    <p>{orderGetData?.shippingInfo?.address}</p>
+                    <p>{orderGetData?.shippingInfo?.state}</p>
+                    <p>{orderGetData?.shippingInfo?.country}</p>
+                    <p>{orderGetData?.shippingInfo?.postcode}</p>
                 </div>
             </div>
 
@@ -77,13 +89,13 @@ export default function OrderViewPage() {
                         </tr>
                     </thead>
                     <tbody>
-                        {order.products.map((product, index) => (
+                        {orderGetData?.orderItems?.map((product, index) => (
                             <tr key={index} className="border-t">
-                                <td className="px-4 py-3">{product.name}</td>
-                                <td className="px-4 py-3">{product.quantity}</td>
-                                <td className="px-4 py-3">${product.price.toFixed(2)}</td>
+                                <td className="px-4 py-3">{product?.product_name}</td>
+                                <td className="px-4 py-3">{product?.quantity}</td>
+                                <td className="px-4 py-3">${product?.price}</td>
                                 <td className="px-4 py-3">
-                                    ${(product.price * product.quantity).toFixed(2)}
+                                    ${(product?.price * product?.quantity)}
                                 </td>
                             </tr>
                         ))}
@@ -93,19 +105,19 @@ export default function OrderViewPage() {
                             <td className="px-4 py-3 text-right" colSpan="3">
                                 Total
                             </td>
-                            <td className="px-4 py-3">${order.total.toFixed(2)}</td>
+                            <td className="px-4 py-3">₹{orderGetData?.totalPrice}</td>
                         </tr>
                         <tr className="border-t font-bold">
                             <td className="px-4 py-3 text-right" colSpan="3">
                                 Shipping Price
                             </td>
-                            <td className="px-4 py-3">${order.total.toFixed(2)}</td>
+                            <td className="px-4 py-3">₹{orderGetData?.shippingPrice}</td>
                         </tr>
                         <tr className="border-t font-bold">
                             <td className="px-4 py-3 text-right" colSpan="3">
                                 Total Amount
                             </td>
-                            <td className="px-4 py-3">${order.total.toFixed(2)}</td>
+                            <td className="px-4 py-3">₹{orderGetData?.totalPrice}</td>
                         </tr>
                     </tfoot>
                 </table>
