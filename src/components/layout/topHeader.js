c0b9f390818fc3174577e9ece223a71ebb1f732f
VelvizhiMani <96263546+VelvizhiMani@users.noreply.github.com>
2025-05-14 00:51:12 +0400

    Update topHeader.js

--- src/components/layout/topHeader.js
+++ src/components/layout/topHeader.js
@@ -1,16 +1,15 @@
 "use client";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import Link from "next/link";
+import LockOpenIcon from "@mui/icons-material/LockOpen";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { IMAGES } from "@/components/common/images";
-import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import { useSelector, useDispatch } from "react-redux";
-import { getProfileService } from "@/services/profileService";
 import { useRouter } from "next/navigation";
-import PersonIcon from "@mui/icons-material/Person";
-import { getCartService } from "@/services/cartService";
 import SearchBar from "../common/SearchBar";
-import { GetSendFeedbackListService } from '@/services/sendFeebackService';
-import LogoutModal from '@/components/common/LogoutModal'
+import { GetSendFeedbackListService } from "@/services/sendFeebackService";
+import LogoutModal from "@/components/common/LogoutModal";
 import { setMenuTab } from "@/reduxToolkit/slices/commonSlice";
 
 const TopHeader = () => {
@@ -18,30 +17,17 @@ const TopHeader = () => {
   const router = useRouter();
   const { profile } = useSelector((state) => state.profileData);
   const dispatch = useDispatch();
-  const [pincode, setPincode] = useState("");
-  const [query, setQuery] = useState("");
   const [isOpen, setIsOpen] = useState(false);
   const [isLogout, setIsLogout] = useState(false);
 
-  const handleSearch = () => {
-    alert(`Searching for: ${query} in pincode: ${pincode}`);
-  };
-
   useEffect(() => {
-    // dispatch(getProfileService());
-    // dispatch(getCartService())
-    dispatch(GetSendFeedbackListService(1, 4))
+    dispatch(GetSendFeedbackListService(1, 4));
   }, []);
 
   const logout = async () => {
     await localStorage.removeItem("token");
     await localStorage.removeItem("cart");
-    // window.location.reload();
-    window.location.href = "/"
-  };
-
-  const loginLink = () => {
-    router.push("/login");
+    window.location.href = "/";
   };
 
   return (
@@ -57,18 +43,22 @@ const TopHeader = () => {
             <SearchBar />
           </div>
           <div className="w-full md:w-2/6 lg:w-3/3">
-            <div className="flex justify-center items-center gap-6">
+            <div className="flex justify-center items-center gap-10">
               <div className="flex">
-                <button
-                  onClick={() => router.push("/prescription-upload")}
-                  type="button"
-                  className="text-white bg-bgcolor hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-1.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55"
-                >
-                  <CloudUploadIcon size="small" className="me-2" /> Upload
-                </button>
-                <div className="relative" onClick={() => router.push('/cart')}>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 text-black mt-1 ml-5 cursor-pointer">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                <div className="relative" onClick={() => router.push("/cart")}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-6 text-black mt-1 cursor-pointer"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                    />
                   </svg>
                   {items?.length > 0 && (
                     <div className="absolute top-0 right-0 left-8 inline-flex items-center justify-center w-5 h-5 bg-red-500 text-white text-xs rounded-full">
@@ -89,6 +79,7 @@ const TopHeader = () => {
                         <AccountCircleIcon className="text-white" />
                         &nbsp; {profile?.username}
                       </button>
+
                       {/* Dropdown Menu */}
                       {isOpen && (
                         <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg z-40">
@@ -96,8 +87,8 @@ const TopHeader = () => {
                             <li
                               className="px-4 py-2 flex items-center gap-2 hover:bg-gray-100 cursor-pointer"
                               onClick={async () => {
-                                await router.push('/profile');
-                                dispatch(setMenuTab('profile'));
+                                await router.push("/profile");
+                                dispatch(setMenuTab("profile"));
                                 setIsOpen(false);
                               }}
                             >
@@ -106,8 +97,8 @@ const TopHeader = () => {
                             <li
                               className="px-4 py-2 flex items-center gap-2 hover:bg-gray-100 cursor-pointer"
                               onClick={async () => {
-                                await router.push('/profile');
-                                dispatch(setMenuTab('orders'));
+                                await router.push("/profile");
+                                dispatch(setMenuTab("orders"));
                                 setIsOpen(false);
                               }}
                             >
@@ -116,8 +107,8 @@ const TopHeader = () => {
                             <li
                               className="px-4 py-2 flex items-center gap-2 hover:bg-gray-100 cursor-pointer"
                               onClick={async () => {
-                                await router.push('/profile');
-                                dispatch(setMenuTab('prescription'));
+                                await router.push("/profile");
+                                dispatch(setMenuTab("prescription"));
                                 setIsOpen(false);
                               }}
                             >
@@ -126,8 +117,8 @@ const TopHeader = () => {
                             <li
                               className="px-4 py-2 flex items-center gap-2 hover:bg-gray-100 cursor-pointer"
                               onClick={async () => {
-                                await router.push('/profile');
-                                dispatch(setMenuTab('refills'));
+                                await router.push("/profile");
+                                dispatch(setMenuTab("refills"));
                                 setIsOpen(false);
                               }}
                             >
@@ -136,14 +127,17 @@ const TopHeader = () => {
                             <li
                               className="px-4 py-2 flex items-center gap-2 hover:bg-gray-100 cursor-pointer"
                               onClick={async () => {
-                                await router.push('/profile');
-                                dispatch(setMenuTab('appointment'));
+                                await router.push("/profile");
+                                dispatch(setMenuTab("appointment"));
                                 setIsOpen(false);
                               }}
                             >
                               Appointments
                             </li>
-                            <li className="px-4 py-2 flex items-center gap-2 text-red-500 hover:bg-gray-100 cursor-pointer" onClick={() => setIsLogout(true)}>
+                            <li
+                              className="px-4 py-2 flex items-center gap-2 text-red-500 hover:bg-gray-100 cursor-pointer"
+                              onClick={() => setIsLogout(true)}
+                            >
                               Logout
                             </li>
                           </ul>
@@ -156,13 +150,23 @@ const TopHeader = () => {
                       )}
                     </>
                   ) : (
-                    <button
-                      type="button"
-                      className="text-white bg-bgcolor hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-1.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55"
-                      onClick={loginLink}
-                    >
-                      LogIn 🔒
-                    </button>
+                    <div className="flex items-center space-x-3 text-sm font-medium text-gray-700">
+                      <Link
+                        href="/login"
+                        className="flex items-center bg-bgcolor py-1 px-2 rounded-lg text-white  hover:text-white hover:bg-green-600 transition"
+                      >
+                        <LockOpenIcon className="text-white" />
+                        &nbsp; Login
+                      </Link>
+                      <span className="text-gray-400">|</span>
+                      <Link
+                        href="/register"
+                        className="flex items-center bg-bgcolor py-1 px-2 rounded-lg text-white hover:text-white hover:bg-green-600 transition"
+                      >
+                        <AccountCircleIcon className="text-white" />
+                        &nbsp; Signup
+                      </Link>
+                    </div>
                   )}
                 </div>
               </div>
