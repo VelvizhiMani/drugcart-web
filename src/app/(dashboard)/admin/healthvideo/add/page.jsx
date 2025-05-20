8e6f4f2021ae24f7e516dfd139d42ecaba197ebf
joseferrar <joseferrar4@gmail.com>
2025-05-20 20:13:27 +0530

    health video url issue fixed

--- src/app/(dashboard)/admin/healthvideo/add/page.jsx
+++ src/app/(dashboard)/admin/healthvideo/add/page.jsx
@@ -23,10 +23,8 @@ function HeathVideoAdd() {
     const dispatch = useDispatch()
 
     const URLText = (text) => {
-        const splitText = text.split(" ")
-        const joinSpace = splitText.join("-").toLowerCase()
-        return joinSpace
-    }
+        return text.trim().replace(/[^\w\s-]/g, "").split(/\s+/).join("-").toLowerCase();
+    };
 
     const formik = useFormik({
         initialValues: {
